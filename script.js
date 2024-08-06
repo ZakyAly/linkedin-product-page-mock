e0f722f07131fadd436ae54fe3176a1bea1096c2
zaky aly <zaky3ly@gmail.com>
2024-08-06 23:55:29 +0200

    changing cursor

--- script.js
+++ script.js
@@ -26,9 +26,51 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "flex";
 }
 
+// var numberOfTabs = document.getElementsByClassName("main-tabs").length;
+
+// for (i = 0; i < numberOfTabs; i++) {
+//   document
+//     .getElementsByClassName("tab-link")
+//     [i].addEventListener("click", function () {
+//       alert("Hey you!");
+//     });
+// }
+
+// function activeLink() {
+//   let i;
+
+//   let numberOfTabs = document.getElementsByClassName("tab-link").length;
+
+//   for (i = 0; i < numberOfTabs; i++) {
+//     document.getElementsByClassName("tab-link")[i].classList.add("active-tab");
+//   }
+// }
+
+// function inactiveLink() {
+//   let i;
+
+//   let numberOfTabs = document.getElementsByClassName("tab-link").length;
+
+//   for (i = 0; i < numberOfTabs; i++) {
+//     document
+//       .getElementsByClassName("tab-link")
+//       [i].classList.remove("active-tab");
+//   }
+// }
+
+// function activeLink() {
+//   document.getElementById("home-tab").classList.add("active-tab");
+//   document.getElementById("about-tab").classList.add("active-tab");
+//   document.getElementById("product-tab").classList.add("active-tab");
+//   document.getElementById("posts-tab").classList.add("active-tab");
+//   document.getElementById("jobs-tab").classList.add("active-tab");
+//   document.getElementById("life-tab").classList.add("active-tab");
+//   document.getElementById("people-tab").classList.add("active-tab");
+// }
+
 document.querySelectorAll("li.tab-link").forEach(function (clickedTab) {
   clickedTab.addEventListener("click", function (event) {
-    event.preventDefault();
+    // event.preventDefault();
     document.querySelectorAll("li.tab-link").forEach(function (currentTab) {
       currentTab.classList.remove("active-tab");
       clickedTab.classList.add("active-tab");
